492055c9073a627067df1cebf70e50e26e4828d9
Ruben <rrosales@outbrain.com>
2016-11-30 18:54:11 -0500

    print script

--- routes/index.js
+++ routes/index.js
@@ -13,14 +13,14 @@ function loadExcel(){
 if(typeof require !== 'undefined') XLSX = require('xlsx');
 var workbook = XLSX.readFile('test.xlsx');
 var sheet_name_list = workbook.SheetNames;
-
+var store = {};
 
 
 sheet_name_list.forEach(function(y) { /* iterate through sheets */
   var worksheet = workbook.Sheets[y];
   var campaign_id =0,prev_campaign_id = 0;
   var pixels = [];
-  var store = {};
+  
   for (z in worksheet) {
   
     /* all keys that do not begin with "!" correspond to cell addresses */
@@ -54,11 +54,29 @@ sheet_name_list.forEach(function(y) { /* iterate through sheets */
   console.log(store);
 });
 	
-	
+	printScript(store);
 }
 
 
+function printScript(store){
 
+  for (key in store){
+    var script = "php update_campaign_settings.php impressionPixels ";
+    var urls = "'";
+    var i = 0;
+      for(value in store[key]){
+        if(i == 1){
+          urls += " "
+        }
+        urls += store[key][value].replace(/['"]+/g, '');
+        i+=1;
+
+      }
+      urls+="'";
+      script += urls + " " + key;
+    console.log(script);
+  }
+}
 
 function isNumeric(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
