cee19d819b0ef7e8ef92a754a1f3ccf7b79699cd
Ruben <rrosales@outbrain.com>
2017-01-19 11:41:40 -0500

    Added flex

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 var cheerio = require('cheerio');
-/* GET users listing. */
-var cmd = require('node-cmd');
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
@@ -28,16 +26,10 @@ function loadExcel() {
       /* all keys that do not begin with "!" correspond to cell addresses */
       if (z[0] === '!') continue;
       if (isNumeric(JSON.stringify(worksheet[z].v))) {
-        //console.log((JSON.stringify(worksheet[z].v)))
         prev_campaign_id = campaign_id;
         campaign_id = JSON.stringify(worksheet[z].v);
         if (pixels.length > 0 && prev_campaign_id != 0 && prev_campaign_id !== campaign_id) {
-          // console.log(campaign_id)
-          // console.log(prev_campaign_id)
-          // console.log(pixels)
-          console.log("id: " + prev_campaign_id)
           store[prev_campaign_id] = printScript(pixels, prev_campaign_id);
-          //  = pixels;
           pixels = [];
         }
       } else {
@@ -54,50 +46,40 @@ function loadExcel() {
 }
 
 function getSRC(tag) {
-  try {
-    if(isUrl(tag)){
-      console.log("true")
-      return tag;
-    }
-    else{
-      console.log("false;" + tag.toString())
-    }
+  if (isUrl(tag)) {
+    return tag;
+  } else {
+    console.log("false;" + tag.toString())
   }
-  catch(err){console.log(err)}
-
   try {
     var tags = multipleTags(tag);
-    if(tags){
-      console.log("Succ")
+    if (tags) {
       return tags;
+    } else {
+      console.log("err " + tag)
     }
-     else{console.log("err " + tag)}
-   }
-
-   catch(err){console.log("tag errors")}
-  
-
- 
- return 0;
-
-
+  } catch (err) {
+    console.log("tag errors")
+  }
+  try {} catch (err) {
+    console.log(err)
+  }
+  return '';
 }
 
-function multipleTags(tags){
+function multipleTags(tags) {
   try {
     var AllSRC = '';
     var tempSrc;
     var $ = cheerio.load('')
     console.log(tags);
     $(tags).each(function(i, elem) {
-    console.log("in")
-    console.log($(this).attr('src'))
+      console.log("in")
+      console.log($(this).attr('src'))
       tempSrc = $(this).html().trim();
       AllSRC += $(this).attr('src') || $(tempSrc).attr('src');
-
     });
-    console.log("success: "  + AllSRC)
-    
+    console.log("success: " + AllSRC)
   } catch (err) {
     console.log("for each fail")
     return '';
@@ -106,30 +88,20 @@ function multipleTags(tags){
   }
 }
 
-function isUrl(tag){
-  try{
-    if(validURL(tag)){
+function isUrl(tag) {
+  try {
+    if (isValidURL(tag)) {
       return true;
     }
-    else return false;
-  }
-  catch(err){
+    return false;
+  } catch (err) {
     return false;
   }
 }
 
-function validURL(str) {
-  var pattern = new RegExp('^(https?:\/\/)?' + // protocol
-    '((([a-z\d]([a-z\d-]*[a-z\d])*)\.)+[a-z]{2,}|' + // domain name
-    '((\d{1,3}\.){3}\d{1,3}))' + // OR ip (v4) address
-    '(\:\d+)?(\/[-a-z\d%_.~+]*)*' + // port and path
-    '(\?[;&a-z\d%_.~+=-]*)?' + // query string
-    '(\#[-a-z\d_]*)?$', 'i'); // fragment locater
-  if (!pattern.test(str)) {
-    return false;
-  } else {
-    return true;
-  }
+function isValidURL(s) {
+  var regexp = new RegExp('^(?:[a-z]+:)?//', 'i');
+  return regexp.test(s);
 }
 
 function printScript(pixels, campaign_id) {
@@ -137,18 +109,18 @@ function printScript(pixels, campaign_id) {
   var limiter = 1;
   var temp = "'";
   for (idx in pixels) {
-    //dummy load will remove
     var $ = cheerio.load('');
     var js1 = cheerio.load(pixels[idx].trim())
     var src = getSRC(pixels[idx])
     console.log("-------------------")
     console.log(src + " " + campaign_id)
     console.log("---------1111111----------")
-    if (src) {
-      temp += src;
-    } else {
-      temp += pixels[idx];
+    if (src == '') {
+      console.log("null")
     }
+
+    temp += src;
+
     if (limiter < pixels.length) {
       temp += " ";
     }
@@ -159,7 +131,6 @@ function printScript(pixels, campaign_id) {
 }
 
 function printScriptHelper(script) {
-//  console.log(script);
   return script;
 }
 
